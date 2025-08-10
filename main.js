748397f19af24c85c2f818b82ff179db9a38acef
Shakthivel <shakthi@scripteco.com>
2025-08-10 16:29:57 +0530

    Initail commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import express from "express"
+const app = express()
+
+// Middleware for parsing
+app.use(express.json())
+app.use(express.urlencoded( { extended: true }));
+
+// Route to handle form POST
+app.post("/register", (req, res) => {
+    console.log(req.body)
+    res.send(`User ${req.body.username} registered!`);
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"));
